7de19e448d4e9fe379d98c5731f629dd805151a8
AdamF06 <qq2841864141@gmail.com>
2020-01-19 17:01:18 +1100

    initialzed

---
+++ js/jump.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    let but = document.getElementsByTagName("button")
+
+
+    document.getElementById("resume").onclick = function () {
+        window.location.href = "resume.html";
+    }
+    document.getElementById("home").onclick = function () {
+        window.location.href = "MainPage.html";
+    }
+    document.getElementById("services").onclick = function () {
+        window.location.href = "services.html";
+    }
+    document.getElementById("contact").onclick = function () {
+        window.location.href = "contact.html";
+    }
+}
+
+
+
